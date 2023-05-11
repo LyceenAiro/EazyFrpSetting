6a06da435a319711708a15e6e711d93f78b01bf0
LyceenAiro <1534012710@qq.com>
2023-05-11 21:48:58 +0800

    vtest_4：实现页面UI

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QMainWindow, QWidget, QStackedWidget, QPushButton, QVBoxLayout, QApplication
 from ui.main_ui import Ui_MainWindow
 from signal.main_signal import my_signal
+from PySide6.QtCore import Qt
 
 class MainWindow(QMainWindow):
+
+    ##
+    ## 主窗口设置
+    ##
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
+        self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.ui.setupUi(self)
         self.band()
 
+    ##
+    ## 对象绑定设置
+    ##
     def band(self):
+        # left
+        self.ui.page_server.clicked.connect(self.setserver)
+        self.ui.page_link.clicked.connect(self.setlink)
+
+        # server
         self.ui.server_save.clicked.connect(self.server_save)
 
+        # window
+        self.ui.window_mini.clicked.connect(self.showMinimized)
+        self.ui.window_close.clicked.connect(self.close)
+    
+    ## 
+    ## 设置页面
+    ##
+    def setserver(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def setlink(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+    
+    ##
+    ## 模块
+    ##
     def server_save(self):
         ip = self.ui.server_IP.text()
         port = self.ui.server_Port.text()
         token = self.ui.server_token.text()
         print(f"ip:{ip}\nport:{port}\ntoken:{token}")
 
+    # def mousePressEvent(self, event):
+    #     if event.button() == Qt.
+
+
 if __name__ == "__main__":
-    app = QApplication([]) # 启动一个应用
-    window = MainWindow() # 实例化主窗口
-    window.show() # 展示主窗口
-    app.exec() # 应用自循环 
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
--- ui/main_ui.py
+++ ui/main_ui.py
@@ -15,39 +15,58 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
-    QMainWindow, QPushButton, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QStackedWidget, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(660, 454)
+        MainWindow.resize(800, 500)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.verticalSpacer = QSpacerItem(20, 153, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.verticalLayout_2.addItem(self.verticalSpacer)
-
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.horizontalLayout_4.addItem(self.horizontalSpacer_2)
-
-        self.verticalLayout = QVBoxLayout()
+        self.widge_left = QWidget(self.centralwidget)
+        self.widge_left.setObjectName(u"widge_left")
+        self.widge_left.setGeometry(QRect(10, 10, 120, 481))
+        self.page_link = QPushButton(self.widge_left)
+        self.page_link.setObjectName(u"page_link")
+        self.page_link.setGeometry(QRect(0, 30, 121, 24))
+        self.page_server = QPushButton(self.widge_left)
+        self.page_server.setObjectName(u"page_server")
+        self.page_server.setGeometry(QRect(0, 0, 121, 24))
+        self.window_close = QPushButton(self.widge_left)
+        self.window_close.setObjectName(u"window_close")
+        self.window_close.setGeometry(QRect(0, 420, 121, 24))
+        self.window_mini = QPushButton(self.widge_left)
+        self.window_mini.setObjectName(u"window_mini")
+        self.window_mini.setGeometry(QRect(0, 450, 121, 24))
+        self.line = QFrame(self.centralwidget)
+        self.line.setObjectName(u"line")
+        self.line.setGeometry(QRect(130, 10, 20, 481))
+        self.line.setFrameShape(QFrame.VLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+        self.stackedWidget = QStackedWidget(self.centralwidget)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.stackedWidget.setGeometry(QRect(150, 10, 641, 481))
+        self.stackedWidget.setMouseTracking(False)
+        self.stackedWidget.setContextMenuPolicy(Qt.NoContextMenu)
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.layoutWidget = QWidget(self.page)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(140, 140, 202, 116))
+        self.verticalLayout = QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.label = QLabel(self.centralwidget)
+        self.label = QLabel(self.layoutWidget)
         self.label.setObjectName(u"label")
 
         self.horizontalLayout.addWidget(self.label)
 
-        self.server_IP = QLineEdit(self.centralwidget)
+        self.server_IP = QLineEdit(self.layoutWidget)
         self.server_IP.setObjectName(u"server_IP")
 
         self.horizontalLayout.addWidget(self.server_IP)
@@ -57,12 +76,12 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.label_2 = QLabel(self.centralwidget)
+        self.label_2 = QLabel(self.layoutWidget)
         self.label_2.setObjectName(u"label_2")
 
         self.horizontalLayout_2.addWidget(self.label_2)
 
-        self.server_Port = QLineEdit(self.centralwidget)
+        self.server_Port = QLineEdit(self.layoutWidget)
         self.server_Port.setObjectName(u"server_Port")
 
         self.horizontalLayout_2.addWidget(self.server_Port)
@@ -72,12 +91,12 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.label_3 = QLabel(self.centralwidget)
+        self.label_3 = QLabel(self.layoutWidget)
         self.label_3.setObjectName(u"label_3")
 
         self.horizontalLayout_3.addWidget(self.label_3)
 
-        self.server_token = QLineEdit(self.centralwidget)
+        self.server_token = QLineEdit(self.layoutWidget)
         self.server_token.setObjectName(u"server_token")
 
         self.horizontalLayout_3.addWidget(self.server_token)
@@ -85,34 +104,34 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout_3)
 
-        self.server_save = QPushButton(self.centralwidget)
+        self.server_save = QPushButton(self.layoutWidget)
         self.server_save.setObjectName(u"server_save")
 
         self.verticalLayout.addWidget(self.server_save)
 
-
-        self.horizontalLayout_4.addLayout(self.verticalLayout)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.horizontalLayout_4.addItem(self.horizontalSpacer)
-
-
-        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
-
-        self.verticalSpacer_2 = QSpacerItem(20, 153, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.verticalLayout_2.addItem(self.verticalSpacer_2)
-
+        self.stackedWidget.addWidget(self.page)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.lineEdit = QLineEdit(self.page_2)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(40, 110, 113, 20))
+        self.stackedWidget.addWidget(self.page_2)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+
+        self.stackedWidget.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Frp\u5feb\u901f\u914d\u7f6e", None))
+        self.page_link.setText(QCoreApplication.translate("MainWindow", u"\u914d\u7f6e\u94fe\u63a5", None))
+        self.page_server.setText(QCoreApplication.translate("MainWindow", u"\u670d\u52a1\u5668\u5730\u5740", None))
+        self.window_close.setText(QCoreApplication.translate("MainWindow", u"\u9000\u51fa", None))
+        self.window_mini.setText(QCoreApplication.translate("MainWindow", u"\u6700\u5c0f\u5316", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u670d\u52a1\u5668\u5730\u5740", None))
         self.server_IP.setPlaceholderText(QCoreApplication.translate("MainWindow", u"127.0.0.1", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u670d\u52a1\u5668\u7aef\u53e3", None))
@@ -120,5 +139,6 @@ class Ui_MainWindow(object):
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u670d\u52a1\u5668\u5bc6\u94a5", None))
         self.server_token.setPlaceholderText(QCoreApplication.translate("MainWindow", u"token", None))
         self.server_save.setText(QCoreApplication.translate("MainWindow", u"\u4fdd\u5b58", None))
+        self.lineEdit.setText(QCoreApplication.translate("MainWindow", u"\u914d\u7f6e\u94fe\u63a5", None))
     # retranslateUi
 
