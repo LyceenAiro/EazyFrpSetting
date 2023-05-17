333fcc84ccc12b76594af4eea1de4624832e2352
LyceenAiro <1534012710@qq.com>
2023-05-17 11:00:38 +0800

    v2.0.1：修复tags错误

--- makefile/tags.py
+++ makefile/tags.py
@@ -1,6 +1,6 @@
 class tags:
     def __init__(self):
-        self.version = "2.0.0"
-        self.versionaddit = "Aplha"
+        self.version = "2.0.1"
+        self.versionaddit = "Release"
         self.author = "LyceenAiro"
         self.licenes = "MIT"
