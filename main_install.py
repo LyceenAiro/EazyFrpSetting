55f06f2f1b87fb548720a3501cdec0694b4780e9
LyceenAiro <1534012710@qq.com>
2023-06-24 22:40:35 +0800

    编译器更新

--- main_install.py
+++ main_install.py
@@ -1,3 +1,4 @@
+# version 230624
 import os
 from makefile.tags import tags
 
@@ -8,7 +9,12 @@ upx = "upx-4.0.2-win64"
 os.system(f"pyinstaller -F -i ./ui/icon/logo.ico --noconsole --upx-dir={upx} --collect-all charset_normalizer  client.py")
 
 # 输出文件重新命名
-outfile = f"./dist/EFS_client_{tags().version}.exe"
+if tags().versionaddit == "Release":
+    outfile = f"./dist/EFS_client_{tags().version}.exe"
+elif tags().versionaddit in ("alpha", "beta"):
+    outfile = f"./dist/EFS_client_{tags().version}_{tags().versionaddit}.exe"
+else:
+    outfile = f"./dist/EFS_client_{tags().version}_nocp.exe"
 if os.path.exists(outfile):
     print("INFO: delete older file")
     os.remove(outfile)
