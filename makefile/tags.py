76e12e02b6410b0bc266cf58e49722a0bd2fcbc4
LyceenAiro <1534012710@qq.com>
2024-02-28 12:42:44 +0800

    v2.1.2

--- makefile/tags.py
+++ makefile/tags.py
@@ -1,6 +1,6 @@
 class tags:
     def __init__(self):
-        self.version = "2.1.1"
+        self.version = "2.1.2"
         self.versionaddit = "Release"
         self.author = "LyceenAiro"
         self.licenes = "MIT"
