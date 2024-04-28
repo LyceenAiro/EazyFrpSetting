5c8ab7846d24f09e7facd4a323f91059d0d9a9ac
LyceenAiro <1534012710@qq.com>
2024-04-28 16:43:56 +0800

    240428

--- main_install.py
+++ main_install.py
@@ -1,20 +1,24 @@
-# version 240130
+# version 240428
 import os
+import sys
 from makefile.tags import tags
 
 # upx地址
-upx = "upx-4.2.2-win64"
+upx = "upx-4.2.3-win64"
 
 # 编译
 os.system(f"pyinstaller -F -i ./ui/icon/logo.ico --noconsole --upx-dir={upx} --collect-all charset_normalizer  client.py")
 
 # 输出文件重新命名
+py_version = sys.version.split(' ')[0].split(".")[0]+sys.version.split(' ')[0].split(".")[1]
 if tags().versionaddit == "Release":
-    outfile = f"./dist/EFS_client_{tags().version}.exe"
-elif tags().versionaddit in ("alpha", "beta"):
-    outfile = f"./dist/EFS_client_{tags().version}_{tags().versionaddit}.exe"
+    outfile = f"./dist/EFS_client_{tags().version}R{py_version}.exe"
+elif tags().versionaddit == "alpha":
+    outfile = f"./dist/EFS_client_{tags().version}a{py_version}.exe"
+elif tags().versionaddit == "beta":
+    outfile = f"./dist/EFS_client_{tags().version}b{py_version}.exe"
 else:
-    outfile = f"./dist/EFS_client_{tags().version}_nocp.exe"
+    outfile = f"./dist/EFS_client_{tags().version}_nocp_{py_version}.exe"
 if os.path.exists(outfile):
     print("INFO: delete older file")
     os.remove(outfile)
